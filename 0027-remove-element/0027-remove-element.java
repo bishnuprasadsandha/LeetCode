c91a45d3c73e3560437e0bc80ab5425c4b4e8454
Bishnu Prasad Sandha <asishkumar95889@gmail.com>
2025-09-25 17:15:23 +0530

    Time: 1 ms (3.94%), Space: 42.7 MB (9.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,11 +1,18 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i=0;
-        for(int num : nums){
-            if(num != val){
-                nums[i++] = num;
-            }
+        
+        Arrays.sort(nums);
+        int i = 0, n = nums.length, valStartInd = -1;
+
+        while(i < n && nums[i] <= val) {
+            if(valStartInd == -1 && nums[i] == val) valStartInd = i;
+            i += 1;
         }
-        return i;
+
+        if(valStartInd == -1) return n;
+        
+        int elements = n - (i - valStartInd);
+        while(i < n && valStartInd < n) nums[valStartInd++] = nums[i++];
+        return elements;
     }
 }
